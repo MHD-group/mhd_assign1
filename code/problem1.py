a7c8e14c1a6603ef3262e599a956e0eb00d78ba9
Junyi Xu <junyixu0@gmail.com>
2023-03-19 13:21:50 +0800

    问题1

---
+++ code/problem1.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2023 junyi <junyi@ArchSurface>
+#
+# Distributed under terms of the MIT license.
+"""
+problem1.py
+"""
+
+import numpy as np
+from numpy import arange, cos, sqrt
+from numpy import pi as π
+from matplotlib import pyplot as plt
+
+θ = arange(0, 2 * π, 0.01)
+
+fig, axs = plt.subplots(1,
+                        3,
+                        figsize=(16, 4),
+                        subplot_kw={'projection': 'polar'})
+for (s, i) in zip([0.5, 1, 2], range(3)):
+    s_Δ = sqrt((s**2 + 1)**2 - 4 * s**2 * cos(θ)**2)
+    bn = np.abs(cos(θ))
+    cf = sqrt(0.5 * (s**2 + 1 + s_Δ))
+    cs = sqrt(0.5 * (s**2 + 1 - s_Δ))
+    axs[i].plot(θ, cf, label="cf")
+    axs[i].plot(θ, cs, label="cs")
+    axs[i].plot(θ, bn, label="bn")
+# plt.show()
+axs[2].legend()
+plt.savefig('../figures/problem1.pdf', bbox_inches='tight')
