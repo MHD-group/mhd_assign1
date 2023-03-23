4c81d1c2a6c661dc04c5d6268aa109e24b4e5861
Myron <yh131996@mail.ustc.edu.cn>
2023-03-24 00:13:19 +0800

    [COMMENT]:add comment for code and update graph

--- code/problem3.py
+++ code/problem3.py
@@ -3,7 +3,7 @@
 # vim:fenc=utf-8
 #
 # Created On  : 2023-03-23 06:27
-# Last Modified : 2023-03-23 09:05
+# Last Modified : 2023-03-24 00:11
 # Copyright © 2023 myron <yh131996@mail.ustc.edu.cn>
 #
 # Distributed under terms of the MIT license.
@@ -31,19 +31,20 @@ for (θ, i) in zip([π/24, π/12, π/6], range(3)):
     s2 = (s0 + 1) / 2
     print(s0, s1, s2)
 
+    # hf1 and hf2 is the plot range of hf
     hf1 = (2*sin(θ) / (γ - 1))
     hf2 = (1 / (2*(γ - 1) - 0.5 * γ**2 * sin(θ)**2)) * \
             (sin(θ) * (2-γ) * (1 + s1) + \
             2*cos(θ) * sqrt((γ - 1) * (1 - s1)**2 + s1 * γ**2 * sin(θ)**2))
     print(hf1, hf2)
 
+    # plot the left part of hf
     hf = arange(0, hf1, 0.0001)
 
     B = (γ/2) * hf * sin(θ) - (1-s2)
     C = 2*sin(θ) - (γ - 1) * hf
     Rx = B**2 + C * (hf + 2*s2*sin(θ))
-    # when C == 0
-    # hf =
+
     Xf1 = (B + sqrt(Rx))/C
     axs[i].plot(hf, Xf1, label="Xf⁺/hf S>")
 
@@ -52,21 +53,20 @@ for (θ, i) in zip([π/24, π/12, π/6], range(3)):
     Xf2 = (B + sqrt(Rx))/C
     axs[i].plot(hf, Xf2, label="Xf⁺/hf S<")
 
-    hf = arange(hf1+0.001, hf2, 0.0001)
+    # plot the right part of hf
+    hf = arange(hf1+0.0001, hf2, 0.0001)
     B = (γ/2) * hf * sin(θ) - (1-s1)
     C = 2*sin(θ) - (γ - 1) * hf
     Rx = B**2 + C * (hf + 2*s1*sin(θ))
-    # when C == 0
-    # hf =
     Xf1 = (B + sqrt(Rx))/C
     Xf2 = (B - sqrt(Rx))/C
     axs[i].plot(hf, Xf1, label="Xf⁺/hf S<")
     axs[i].plot(hf, Xf2, label="Xf⁻/hf S<")
 
     axs[i].set_yscale("log", base=10)
-    axs[i].set_ylim(0,1000)
+    axs[i].set_ylim(0.1,1000)
     axs[i].set_title("θ = " + str(int(θ*180/π)) + "°")
 
 plt.show()
 axs[2].legend()
-#plt.savefig('../figures/problem1.pdf', bbox_inches='tight')
+plt.savefig('../figures/problem3.pdf', bbox_inches='tight')
