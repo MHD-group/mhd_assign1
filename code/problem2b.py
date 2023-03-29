c6ef70b7b0ad7d5fc08da83f6a363d22c96f16e6
Junyi Xu <junyixu0@gmail.com>
2023-03-29 12:20:59 +0800

    [Modified]: tex 修改第二幅图;  删掉plt.show()

--- code/problem2b.py
+++ code/problem2b.py
@@ -13,14 +13,12 @@ from numpy import arange, cos, sqrt, sin, maximum, clip, arctan
 from numpy import pi as π
 from matplotlib import pyplot as plt
 
-fig, axs = plt.subplots(1,
-                        2,
-                        figsize=(10, 5))
+fig, axs = plt.subplots(1, 2, figsize=(10, 5))
 step = 0.005
 x = np.arange(0.1, 6, step)
 y = np.arange(0.1, 3, step)
 
-X,Y = np.meshgrid(x, y)
+X, Y = np.meshgrid(x, y)
 
 for (λ, i) in zip([0.5, 2], range(2)):
     Z = -(((Y**2 - λ**2)*(-X**2*(-1 + Y) + Y*(-Y + Y**2 - λ**2))*\
@@ -30,12 +28,12 @@ for (λ, i) in zip([0.5, 2], range(2)):
 
     Z = arctan(sqrt(maximum(0, Z)))
     Z = np.where(Z, Z, np.nan)
-    axi = axs[i].contour(X, Y, Z*180/π, linewidth=0.01)
-    axi = axs[i].pcolormesh(X, Y, Z*180/π, alpha=0.7)
+    axi = axs[i].contour(X, Y, Z * 180 / π, linewidth=0.01)
+    axi = axs[i].pcolormesh(X, Y, Z * 180 / π, alpha=0.7)
     axs[i].set_xlabel("$kc/\omega_c$")
     axs[i].set_ylabel("$\omega/\omega_c$")
-    axs[i].set_title("$\omega_p/\omega_c = "+str(λ)+"$")
-    fig.colorbar(axi, ax = axs[i], label="$\\theta(0°-90°)$")
+    axs[i].set_title("$\omega_p/\omega_c = " + str(λ) + "$")
+    fig.colorbar(axi, ax=axs[i], label="$\\theta(0°-90°)$")
 
-plt.show()
+# plt.show()
 plt.savefig('../figures/problem2b.pdf', bbox_inches='tight')
